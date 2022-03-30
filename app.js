6ec4dea654eba173ddf6f453788af204a57bd2cc
Gs-soares <guilherme.castro@linx3.com>
2022-03-30 16:26:27 -0300

    a

--- app.js
+++ app.js
@@ -1,16 +1,16 @@
-const express = require('express');
-const puppeteer = require('puppeteer');
-const fs = require('fs');
-const path = require('path');
+const express = require("express");
+const puppeteer = require("puppeteer");
+const fs = require("fs");
+const path = require("path");
 
-function createHTML(dataHTML){
+function createHTML(dataHTML) {
   return new Promise((resolve, reject) => {
-      fs.readFile(dataHTML, 'utf8', (err, data) => {
-          if(err) {
-              reject(err);
-          }
-          resolve(data);
-      });
+    fs.readFile(dataHTML, "utf8", (err, data) => {
+      if (err) {
+        reject(err);
+      }
+      resolve(data);
+    });
   });
 }
 
@@ -19,43 +19,46 @@ const app = express();
 const PORT = process.env.PORT || 8877;
 
 app.listen(PORT, () => {
-    console.log(PORT)
-})
+  console.log(PORT);
+});
 
-app.get('/', (req, res) => {
-    res.json({
-        msg: 'ok'
-    })
-})
-
-app.get('/:name', async (req, res) => {
-    console.time()
-    const id = req.params.name;
-    if(fs.existsSync(`pages/${id}.html`)){
-      console.log('passou aqui')
-      return res.sendFile(path.join(__dirname, `/pages/${id}.html`))
-    }
-    console.log('baixando html')
-    const browser = await puppeteer.launch();
-    const page = await browser.newPage();
-    await page.goto(`https://pt.wikipedia.org/wiki/${id}`);
-    
-    const pageData = await page.evaluate(() => {
-      return document.querySelector('#bodyContent').innerText;
-    });
-    // `${document.querySelector('#firstHeading').innerText} 
-    // ${document.querySelector('#bodyContent').innerHTML}`
-    await browser.close();
-
-    fs.writeFile(`pages/${id}.html`, JSON.stringify(pageData), (err) => {
-      if(err) throw err
-      console.log('salvo')
-    })
-
-    console.timeEnd();
-  
-    return res.sendFile(path.join(__dirname, `/pages/${id}.html`))
+app.get("/", (req, res) => {
+  res.json({
+    msg: "ok",
   });
-  
-  
-  app.listen(3000);
+});
+
+app.get("/:name", async (req, res) => {
+  const id = req.params.name;
+  if (fs.existsSync(`pages/${id}.html`)) {
+    return res.sendFile(path.join(__dirname, `/pages/${id}.html`));
+  }
+  const browser = await puppeteer.launch();
+  const page = await browser.newPage();
+  await page.goto(`https://pt.wikipedia.org/wiki/${id}`);
+
+  const pageData = await page.evaluate(() => {
+    return document.querySelector("#content").innerText;
+  });
+
+  await browser.close();
+
+  var originalWord = pageData;
+  if(originalWord.includes("\n")){
+    var newWord = originalWord.replaceAll("\n", " ");
+    newWord = newWord.replaceAll("\t", " ");
+  }
+
+  fs.writeFile(`pages/${id}.html`, JSON.stringify(newWord), (err) => {
+    if (err) throw err;
+    console.log("salvo");
+  });
+
+  // fs.writeFile(`pages/${id}.html`, JSON.stringify(pageData), (err) => {
+  //   if(err) throw err
+  //   console.log('salvo')
+  // })
+  return res.sendFile(path.join(__dirname, `/pages/${id}.html`));
+});
+
+app.listen(3000);
